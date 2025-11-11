fcc98764cf4944470c02f446365300dbe8b53085
fatiyev <sahibbffati@gmail.com>
2025-11-12 02:35:18 +0400

    fix

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""to_json_string
+"""to_json_string module
 """
-
 
 import json
 
 
 def to_json_string(my_obj):
-    """Return JSON format of my_obj
-    """
-    
+    """Return JSON representation of my_obj."""
     return json.dumps(my_obj)
