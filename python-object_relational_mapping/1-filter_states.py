507d5f9cb616c3f1d2d1e5c7d100b1f5b0fbf84a
fatiyev <sahibbffati@gmail.com>
2025-12-02 15:39:43 +0400

    fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -13,21 +13,26 @@ def get_states_n():
         argv[2]: mysql password
         argv[3]: database name
     """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+    try:
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
 
-    cur = db.cursor()
+        cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
-    rows = cur.fetchall()
-    for i in rows:
-        print(i)
+        cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+        rows = cur.fetchall()
+        for i in rows:
+            print(i)
 
-    cur.close()
-    db.close()
+        cur.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print(f"MySQL Error: {e}")
+
 
 if __name__ == "__main__":
     get_states_n()
