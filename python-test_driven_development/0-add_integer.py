d45efe9c05bddf587c5cb989066687a6183573aa
fatiyev <sahibbffati@gmail.com>
2025-12-06 03:41:56 +0400

    hotfix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,6 +4,7 @@ add_integer:
     Checks if parameters are int
     Returns sum of parameters
 """
+import math
 
 
 def add_integer(a, b=98):
@@ -16,8 +17,12 @@ def add_integer(a, b=98):
         raise TypeError("b must be an integer")
 
     if type(a) is float:
+        if math.isnan(a):
+            raise ValueError("cannot convert float NaN to integer")
         a = int(a)
     if type(b) is float:
+        if math.isnan(b):
+            raise ValueError("cannot convert float NaN to integer")
         b = int(b)
 
     return a + b
