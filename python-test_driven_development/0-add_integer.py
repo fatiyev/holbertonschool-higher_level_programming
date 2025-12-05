addf69687f46391ad60614b82f7ceca30840cf5c
fatiyev <sahibbffati@gmail.com>
2025-12-06 03:47:12 +0400

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,9 +10,9 @@ def add_integer(a, b=98):
     """
     Checks if int or float (but not bool), convert floats to int, otherwise raise.
     """
-    if type(a) is not int and type(a) is not float:
+    if type(a) is bool or (type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
-    if type(b) is not int and type(b) is not float:
+    if type(b) is bool or (type(b) is not int and type(b) is not float):
         raise TypeError("b must be an integer")
 
     if type(a) is float:
